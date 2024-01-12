a29f47557b094afd07598edc5ff665df94a62cf9
中了5毛钱 <o_qll@outlook.com>
2024-01-12 16:21:25 +0800

    feat: public 1.0.0

--- src/core/index.ts
+++ src/core/index.ts
@@ -1,14 +1,29 @@
+
+const getTtime = () => {
+  return new Date().toLocaleTimeString()
+}
+
+const showLog = () => {
+  const flag = localStorage.getItem("dslog");
+  return flag === "true";
+}
+
 export const dslog = {
   info: (message: string) => {
-    console.log(`%cInfo: ${message}`, "color: #56b97f");
+    if (!showLog()) return
+    console.log(`%c [ ${getTtime()} ] [ INFO ] ${message}`, "color: #56b97f");
   },
   method: (message: string) => {
-    console.log(`%cMethod: ${message}`, "color: #56b97f");
+    if (!showLog()) return
+    console.log(`%c [ ${getTtime()} ] [ METH ] ${message} `, "color: #c0a4d8");
   },
   obj: (object: object) => {
-    console.log("Object:", object);
+    if (!showLog()) return
+    const deepCopyObj = JSON.parse(JSON.stringify(object))
+    console.log(`%c [ ${getTtime()} ] [ OBJE ] `, "color: yellow", deepCopyObj);
   },
   error: (message: string) => {
-    console.log(`%cError: ${message}`, "color: red");
+    if (!showLog()) return
+    console.log(`%c [ ${getTtime()} ] [ ERRO ] ${message} `, "color: red");
   },
 };
