4bea0f8edad09ac9a76ed8382502966a3bef6b79
中了5毛钱 <o_qll@outlook.com>
2024-02-21 15:21:31 +0800

    feat: 增加背景, 支持 多参数输入

--- dist/dslog.d.ts
+++ dist/dslog.d.ts
@@ -1,8 +1,8 @@
 declare const dslog: {
-    info: (message: any) => void;
-    method: (message: any) => void;
-    obj: (object: object) => void;
-    error: (message: any) => void;
+    info: (...args: any[]) => void;
+    method: (...args: any[]) => void;
+    obj: (...args: any[]) => void;
+    error: (...args: any[]) => void;
 };
 
 export { dslog };
--- src/core/index.ts
+++ src/core/index.ts
@@ -9,21 +9,21 @@ const showLog = () => {
 }
 
 export const dslog = {
-  info: (message: any) => {
-    if (!showLog()) return
-    console.log(`%c [ ${getTtime()} ] [ INFO ] ${message}`, "color: #56b97f");
+  info: (...args: any[]) => {
+    if (!showLog()) return;
+    console.log(`%c[ ${getTtime()} ] [ INFO ]`, "color: #fff; padding: 2px; background-image: linear-gradient(to right, #eea2a2 0%, #bbc1bf 19%, #57c6e1 42%, #b49fda 79%, #7ac5d8 100%); border-radius: 2px", ...args);
   },
-  method: (message: any) => {
-    if (!showLog()) return
-    console.log(`%c [ ${getTtime()} ] [ METH ] ${message} `, "color: #c0a4d8");
+  method: (...args: any[]) => {
+    if (!showLog()) return;
+    console.log(`%c[ ${getTtime()} ] [ METH ]`, "color: #fff; padding: 2px; background-image: linear-gradient(-20deg, #fc6076 0%, #ff9a44 100%); border-radius: 2px", ...args);
   },
-  obj: (object: object) => {
-    if (!showLog()) return
-    const deepCopyObj = JSON.parse(JSON.stringify(object))
-    console.log(`%c [ ${getTtime()} ] [ OBJE ] `, "color: yellow", deepCopyObj);
+  obj: (...args: any[]) => {
+    if (!showLog()) return;
+    console.log(`%c[ ${getTtime()} ] [ OBJE ]`, "color: yellow; padding: 2px; background-color: #303030; border-radius: 2px", ...args.map(obj => JSON.parse(JSON.stringify(obj))));
   },
-  error: (message: any) => {
-    if (!showLog()) return
-    console.log(`%c [ ${getTtime()} ] [ ERRO ] ${message} `, "color: red");
+  error: (...args: any[]) => {
+    if (!showLog()) return;
+    console.log(`%c[ ${getTtime()} ] [ ERRO ]`, "color: #34146A; padding: 2px; background-image: linear-gradient(-225deg, #69EACB 0%, #EACCF8 48%, #6654F1 100%); border-radius: 2px", ...args);
   },
 };
+
